6cd211cc03920af515f4fe5f6b4d64bdfe74a51c
Kyler Smith <kysmith327@gmail.com>
2020-11-28 13:52:29 -0800

    Workshop 2 Final tweaks

--- models/promotion.js
+++ models/promotion.js
@@ -4,27 +4,6 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const commentSchema = new Schema({
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
-
-
-
 const promotionSchema = new Schema({
     name: {
         type: String,
@@ -47,8 +26,7 @@ const promotionSchema = new Schema({
     description: {
         type: String,
         required: true
-    },
-    comments: [commentSchema]
+    }
 }, {
     timestamps: true
 });
