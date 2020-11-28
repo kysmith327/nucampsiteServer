93e0c4e2f2fc8fdfeb0b583bcb37acb6b9afb08f
Kyler Smith <kysmith327@gmail.com>
2020-11-28 12:06:07 -0800

    Add comments to promo/partners

--- models/partner.js
+++ models/partner.js
@@ -4,6 +4,25 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 
 
 const partnerSchema = new Schema({
@@ -23,7 +42,8 @@ const partnerSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
--- models/promotion.js
+++ models/promotion.js
@@ -4,6 +4,26 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+
 
 const promotionSchema = new Schema({
     name: {
@@ -27,7 +47,8 @@ const promotionSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
